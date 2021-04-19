6569020b05b4f4a326bb09bf7ca41c9dbc07f0f0
Ashish Karn <ashishkrkarn@gmail.com>
2021-04-19 15:25:46 +0530

    Add rest data @RestResource

--- src/main/java/karn/ashish/springexperiments/dao/PlayersDao.java
+++ src/main/java/karn/ashish/springexperiments/dao/PlayersDao.java
@@ -1,14 +1,15 @@
 package karn.ashish.springexperiments.dao;
 
+import karn.ashish.springexperiments.pojo.Player;
 import karn.ashish.springexperiments.pojo.Team;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-//http://localhost:8080/teams
-@RestResource(path = "teams", rel = "teams")
-public interface TeamDao extends CrudRepository<Team, Long> {
-    List<Team> findAll();
+//http://localhost:8080/players
+@RestResource(path = "players", rel = "players")
+public interface PlayersDao extends CrudRepository<Player, Long> {
+    List<Player> findAll();
     Team findByName(String name);
 }
